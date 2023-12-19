aeeedd62504ab38b903525e7e9dc7eebaf7034d2
Eduardo Vítor <eduardovitor730@gmail.com>
2023-12-19 19:39:07 -0300

    style: flake8 format

--- main.py
+++ main.py
@@ -6,57 +6,85 @@ import numpy as np
 API_KEY = os.environ["API_TELEGRAM_CONCURSOS"]
 bot = telebot.TeleBot(API_KEY)
 msg = 'Olá, eu sou o ConcursosTIBot'
-msg2 = "Criado por Eduardo Vítor, meu objetivo é trazer concursos de todo o Brasil que ofertam vagas para a área de TI (Tecnologia da informação) do site PCI Concursos",
-msg3 = 'Você têm 4 comandos nesse bot:\n/vagasanalista: concursos com vagas de analista de tecnologia e analista de sistemas\n/vagasprofessor: concursos com vagas de professor de TI efetivo e substituto\n/vagastecnico: concursos com vagas de técnico em informática\n/help: mostra novamente essa ajuda\n'
+msg2 = """Criado por Eduardo Vítor, meu objetivo é trazer concursos do Brasil \
+que ofertam vagas para a área de TI (Tecnologia da informação) \
+do site PCI Concursos""",
+msg3 = """Você têm 4 comandos nesse bot:
+/vagasanalista: concursos para analista de tecnologia e analista de sistemas
+/vagasprofessor: concursos para professor de TI efetivo e substituto
+/vagastecnico: concursos para técnico em informática
+/help: mostra novamente essa ajuda\n"""
+polling_timeout = 50
+
 
 def construir_markup(item1, item2):
-  markup = telebot.types.ReplyKeyboardMarkup(row_width=2)
-  btn1 = telebot.types.KeyboardButton(item1)
-  btn2 = telebot.types.KeyboardButton(item2)
-  markup.add(btn1,btn2)
-  return markup
-  
+    markup = telebot.types.ReplyKeyboardMarkup(row_width=2)
+    btn1 = telebot.types.KeyboardButton(item1)
+    btn2 = telebot.types.KeyboardButton(item2)
+    markup.add(btn1, btn2)
+    return markup
+
+
 @bot.message_handler(commands=['start'])
 def welcome(message):
-  bot.send_message(message.chat.id, msg)
-  bot.send_message(message.chat.id, msg2)
-  bot.send_message(message.chat.id, msg3)
+    bot.send_message(message.chat.id, msg)
+    bot.send_message(message.chat.id, msg2)
+    bot.send_message(message.chat.id, msg3)
+
 
 @bot.message_handler(commands=['help'])
 def help(message):
-  bot.send_message(message.chat.id, msg3)
+    bot.send_message(message.chat.id, msg3)
+
 
 def handle_concursos(msg):
     if msg.text in pci_ti.CARGOS:
         return True
 
+
 @bot.message_handler(commands=['vagasanalista'])
 def vagas_analista(message):
-  markup = construir_markup(pci_ti.CARGOS_ANALISTA[0],pci_ti.CARGOS_ANALISTA[1])
-  bot.send_message(message.chat.id, "Escolha uma opção:",reply_markup=markup)
+    markup = construir_markup(pci_ti.CARGOS_ANALISTA[0],
+                              pci_ti.CARGOS_ANALISTA[1])
+    bot.send_message(message.chat.id, "Escolha uma opção:",
+                     reply_markup=markup)
+
 
 @bot.message_handler(commands=['vagasprofessor'])
 def vagas_professor(message):
-  markup = construir_markup(pci_ti.CARGOS_PROFESSOR[0],pci_ti.CARGOS_PROFESSOR[1])
-  bot.send_message(message.chat.id, "Escolha uma opção:",reply_markup=markup)
+    markup = construir_markup(pci_ti.CARGOS_PROFESSOR[0],
+                              pci_ti.CARGOS_PROFESSOR[1])
+    bot.send_message(message.chat.id, "Escolha uma opção:",
+                     reply_markup=markup)
+
 
 @bot.message_handler(commands=['vagastecnico'])
 def vagas_tecnico(message):
-  markup = construir_markup(pci_ti.CARGOS_TECNICO[0],pci_ti.CARGOS_TECNICO[1])
-  bot.send_message(message.chat.id, "Escolha uma opção:",reply_markup=markup)
+    markup = construir_markup(pci_ti.CARGOS_TECNICO[0],
+                              pci_ti.CARGOS_TECNICO[1])
+    bot.send_message(message.chat.id, "Escolha uma opção:",
+                     reply_markup=markup)
+
 
 @bot.message_handler(func=handle_concursos)
 def get_concursos(message):
     concursos = pci_ti.scrapy_vagas(pci_ti.VAGAS_LINK_DIC[message.text])
     msg_resposta = pci_ti.formatar_msg(concursos)
     try:
-       bot.send_message(message.chat.id,msg_resposta)
+        bot.send_message(message.chat.id, msg_resposta)
     except telebot.apihelper.ApiTelegramException as error:
-       metades = np.array_split(concursos,2)
-       msg_resposta = pci_ti.formatar_msg(metades[0])
-       print(error)
-       bot.send_message(message.chat.id,msg_resposta)
-       bot.send_message(message.chat.id,f"Infelizmente, o Telegram possui um tamanho máximo de mensagem, a quantidade de vagas desse cargo é muito grande, logo não poderão ser exibidas todas aqui, verifique o link https://www.pciconcursos.com.br{pci_ti.VAGAS_LINK_DIC[message.text]} para ver todos os concursos disponíveis para a vaga.")
-    
+        metades = np.array_split(concursos, 2)
+        msg_resposta = pci_ti.formatar_msg(metades[0])
+        print(error)
+        bot.send_message(message.chat.id, msg_resposta)
+        bot.send_message(message.chat.id, f"""Infelizmente, o Telegram possui \
+um tamanho máximo de mensagem, \
+a quantidade de vagas desse cargo é muito grande, \
+logo não poderão ser exibidas todas aqui,\
+verifique o link \
+https://www.pciconcursos.com.br{pci_ti.VAGAS_LINK_DIC[message.text]} \
+para ver todos os concursos \
+disponíveis para a vaga.""")
 
-bot.infinity_polling(50)
+
+bot.infinity_polling(polling_timeout)
--- pci_ti.py
+++ pci_ti.py
@@ -2,46 +2,57 @@ from bs4 import BeautifulSoup
 import requests
 import validators
 
-HEADERS = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'}
+HEADERS = {'User-Agent': """Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+           AppleWebKit/537.36 (KHTML, like Gecko) \
+           Chrome/109.0.0.0 Safari/537.36"""}
 URL = "https://www.pciconcursos.com.br"
 CARGOS_ANALISTA = ("Analista de Tecnologia", "Analista de Sistemas")
-CARGOS_PROFESSOR = ("Professor de informática efetivo", "Professor de informática substituto")
+CARGOS_PROFESSOR = ("Professor de informática efetivo",
+                    "Professor de informática substituto")
 CARGOS_TECNICO = ("Técnico de informática", "Técnico em informática")
 CARGOS = CARGOS_ANALISTA + CARGOS_PROFESSOR + CARGOS_TECNICO
-VAGAS_LINK_DIC =  {
-   CARGOS[0]:"/cargos/analista-de-tecnologia-da-informacao",
-   CARGOS[1]:"/cargos/analista-de-sistemas",
-   CARGOS[2]:"/cargos/professor-de-informatica",
-   CARGOS[3]:"/cargos/professor-substituto-informatica",
-   CARGOS[4]:"/cargos/tecnico-de-informatica",
-   CARGOS[5]:"/cargos/tecnico-em-informatica"
+VAGAS_LINK_DIC = {
+   CARGOS[0]: "/cargos/analista-de-tecnologia-da-informacao",
+   CARGOS[1]: "/cargos/analista-de-sistemas",
+   CARGOS[2]: "/cargos/professor-de-informatica",
+   CARGOS[3]: "/cargos/professor-substituto-informatica",
+   CARGOS[4]: "/cargos/tecnico-de-informatica",
+   CARGOS[5]: "/cargos/tecnico-em-informatica"
 }
 
-def scrapy_vagas(uri):
-  scrapy_url = "{}{}".format(URL,uri)
-  doc_html = requests.get(scrapy_url,headers=HEADERS)
-  soup_html = BeautifulSoup(doc_html.content,'lxml')
-  vagas_html = soup_html.find_all("ul",class_="link-d")
-  dics_concursos = []
-  for vaga in vagas_html:
-        if vaga.li.a.text.isupper()==False:
-          if validar_dados(vaga.li.a["href"],vaga.li.a.text):
-            dic = {"concurso":vaga.li.a.text,"link":vaga.li.a["href"]}
-            dics_concursos.append(dic)
-  return dics_concursos if len(dics_concursos)>0 else "Não há vagas para este cargo disponíveis no momento"
 
-def validar_dados(link,titulo):
+def scrapy_vagas(uri):
+    scrapy_url = "{}{}".format(URL, uri)
+    doc_html = requests.get(scrapy_url, headers=HEADERS)
+    soup_html = BeautifulSoup(doc_html.content, 'lxml')
+    vagas_html = soup_html.find_all("ul", class_="link-d")
+    dics_concursos = []
+    for vaga in vagas_html:
+        if not vaga.li.a.text.isupper():
+            if validar_dados(vaga.li.a["href"], vaga.li.a.text):
+                dic = {"concurso": vaga.li.a.text, "link": vaga.li.a["href"]}
+                dics_concursos.append(dic)
+    return dics_concursos if len(dics_concursos) > 0 else """Não há vagas \
+                                                          para este cargo \
+                                                          disponíveis \
+                                                          no momento"""
+
+
+def validar_dados(link, titulo):
     return validators.url(link) and len(titulo) > 0
 
+
 def formatar_msg(dics_concursos):
-  if isinstance(dics_concursos, str):
-    return dics_concursos
-  else:
-    dic_tam = len(dics_concursos)
-    i = 0
-    dic_msg=[]
-    while(i<dic_tam):
-      dic_msg.append("{}\n\n{}\n\n".format(dics_concursos[i]['concurso'],dics_concursos[i]['link']))
-      i+=1
-    msg="".join(dic_msg)
-  return msg
+    if isinstance(dics_concursos, str):
+        return dics_concursos
+    else:
+        dic_tam = len(dics_concursos)
+        i = 0
+        dic_msg = []
+        while (i < dic_tam):
+            concurso = f"{dics_concursos[i]['concurso']}\n\n"
+            link = f"{dics_concursos[i]['link']}\n\n"
+            dic_msg.append(f"{concurso}{link}")
+            i += 1
+        msg = "".join(dic_msg)
+    return msg
